a5dd08897b2a25d4637303f65c8616f708638381
Marcos891 <MarcosMansilla1989@hotmail.com>
2022-07-12 00:28:43 -0300

    modificaciones

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ const app = express()
 app.use(express.static('public'));
 app.listen(port, () => console.log('server running in localhost://' + port));
 
-app.get('/', (req,res) => res.sendFile(path.resolve(__dirname, 'views', 'home.html')));
-app.get('/', (req,res) => res.sendFile(path.resolve(__dirname, 'views', 'detalleProducto.html')));
-app.get('/', (req,res) => res.sendFile(path.resolve(__dirname, 'views', 'carritoCompra.html')));
-app.get('/', (req,res) => res.sendFile(path.resolve(__dirname, 'views', 'register.html')));
-app.get('/', (req,res) => res.sendFile(path.resolve(__dirname, 'views', 'login.html')));
+app.get('/index', (req,res) => res.sendFile(path.resolve(__dirname, 'views', 'index.html')));
+app.get('/productDetail', (req,res) => res.sendFile(path.resolve(__dirname, 'views', 'productDetail.html')));
+app.get('/productCart', (req,res) => res.sendFile(path.resolve(__dirname, 'views', 'productCart.html')));
+app.get('/register', (req,res) => res.sendFile(path.resolve(__dirname, 'views', 'register.html')));
+app.get('/login', (req,res) => res.sendFile(path.resolve(__dirname, 'views', 'login.html')));
