7728b3aa3b294bff499813db0706da9e580fa724
Marcos891 <MarcosMansilla1989@hotmail.com>
2022-07-27 22:56:40 -0300

    nose

--- app.js
+++ app.js
@@ -5,12 +5,14 @@ const port = 3049;
 const app = express()
 
 app.use(express.static('public'));
-app.listen(port, () => console.log('server running in localhost://' + port));
 
 app.get('/', (req,res) => res.sendFile(path.resolve(__dirname, 'views', 'index.html')));
 app.get('/productDetail', (req,res) => res.sendFile(path.resolve(__dirname, 'views', 'productDetail.html')));
 app.get('/productCart', (req,res) => res.sendFile(path.resolve(__dirname, 'views', 'productCart.html')));
 app.get('/register', (req,res) => res.sendFile(path.resolve(__dirname, 'views', 'register.html')));
 app.get('/login', (req,res) => res.sendFile(path.resolve(__dirname, 'views', 'login.html')));
+
+
+app.listen(port, () => console.log('server running in http//localhost:' + port));
 
 /* Recuerden cuando bajan todo hacer el "npm i express" */
