71558e04c4489296142349871ae9adaa0ad1d7d2
Marcos891 <MarcosMansilla1989@hotmail.com>
2022-08-17 22:46:41 -0300

    add json user

--- src/routes/products.js
+++ src/routes/products.js
@@ -4,16 +4,22 @@ const router = express.Router();
 const {productDetail, productCart, productAdd, productEdit} = require('../controllers/productsController')
 
 router
+            /*  */
     .get('/productDetail', productDetail)
 
+            /*  */
     .get('/productCart', productCart)
 
+            /*  */
     .get('/productAdd', productAdd)
 
+            /*  */
     .get('/productEdit', productEdit)
 
+            /*  */
     .put('/productAdd', productAdd)
 
+            /*  */
     .put('/productEdit', productEdit)
     
 module.exports = router;
