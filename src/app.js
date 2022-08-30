1e946dac6ac1c56320db3b07c1cc19683dbe6daf
lautaroVal <valenzuelalautaro@yahoo.com>
2022-08-29 23:33:52 -0300

    instalación de todos los paquetes

--- src/app.js
+++ src/app.js
@@ -26,7 +26,19 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/products', productsRouter);
 
-
+app.use(function(req, res, next) {
+    next(createError(404));
+  });
+  
+app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+  
+    // render the error page
+    res.status(err.status || 500);
+    res.render('not-found');
+  });
 
 /* app.use('/*', (req, res, next) => {
     try {
