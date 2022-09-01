549ab3dedc032e5cc569d5d10e0385f93b019698
Nahiara <belvederenahiara@gmail.com>
2022-08-31 22:28:09 -0300

    Subo mis cambios

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,8 +1,29 @@
+
+
+const {validationResults} = require('express-validator');
+const bcryptjs = require('bcryptjs');
+
 module.exports = {
     register: (req,res) => {
         return res.render('users/register')
     },
     login: (req,res) => {
         return res.render('users/login')
+    },
+    processLogin: (req,res) => {
+        let errors = validationResult(req);
+
+        if(errors.isEmpy()) {
+            let {id, name, username} = loadUsers().find(user = user.email === req.body.email)
+            
+            return res.redirect('/')
+        } else {
+            return res.render('/login', {
+                title : 'login',
+                errors : errors.maped()
+            })
+        }
+        return res.send(req.body)
+
     }
 }
--- src/routes/users.js
+++ src/routes/users.js
@@ -2,12 +2,13 @@ const { application } = require('express');
 const express = require('express');
 const router = express.Router();
 
-const {register, login} = require('../controllers/usersController');
+const {register, login, processLogin} = require('../controllers/usersController');
+const usersController = require('../controllers/usersController');
+const loginValidator = require('../validations/loginValidator');
 
 router
     .get('/register', register)
     .get('/login', login)
-
-
+    .post('/login', loginValidator, processLogin)
 
 module.exports = router;
