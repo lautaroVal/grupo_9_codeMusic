b1d30fb70dab9005016106ee5663c779488bd14d
lautaroVal <valenzuelalautaro@yahoo.com>
2022-09-12 15:57:43 -0300

    arregle profile

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const registerValidator = require('../validations/registerValidator');
-const loginValidator = require('../validations/loginValidator')
-const profileValidator = require('../validations/profileValidator')
-const uploadImges = require('../middlewares/uploadImg')
+const loginValidator = require('../validations/loginValidator');
+const profileValidator = require('../validations/profileValidator');
+const uploadImges = require('../middlewares/uploadImg');
 
-const userSessionCheck = require('../middlewares/userSessionCheck')
+const userSessionCheck = require('../middlewares/userSessionCheck');
 const {register, login, processRegister, processLogin, profile, logout, update} = require('../controllers/usersController');
 
 
