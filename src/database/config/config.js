64cef0409ff3d675d373593922414d0952368c99
lautaroVal <valenzuelalautaro@yahoo.com>
2022-10-03 18:08:32 -0300

    config

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,9 +1,10 @@
-{
+module.exports = {
   "development": {
-    "username": "root",
-    "password": null,
-    "database": "database_development",
-    "host": "127.0.0.1",
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_DATABASE,
+    "host": process.env.DB_HOST,
+    "port": process.env.DB_PORT,
     "dialect": "mysql"
   },
   "test": {
