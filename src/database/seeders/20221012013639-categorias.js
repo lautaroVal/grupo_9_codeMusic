488e12cfe5b578fdf4049dc5739f14717ae74cd4
lautaroVal <valenzuelalautaro@yahoo.com>
2022-10-12 00:05:18 -0300

    creé los primeros 4 seeders

---
+++ src/database/seeders/20221012013639-categorias.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const categories = [
+  {
+    name : 'oferta',
+    createdAt : new Date()},
+  {
+    name : 'guitarras',
+    createdAt : new Date()},
+  {
+    name : 'baterias',
+    createdAt : new Date()},
+  {
+    name : 'teclados',
+    createdAt : new Date()},
+  {
+    name : 'microfonosYSonidos',
+    createdAt : new Date()},
+  {
+    name : 'deVientos',
+    createdAt : new Date()}
+]
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    
+      //Add seed commands here.
+     
+      //Example:
+      await queryInterface.bulkInsert('Categories', categories, {});
+    
+  },
+
+  async down (queryInterface, Sequelize) {
+    
+      //Add commands to revert seed here.
+     
+      //Example:
+      await queryInterface.bulkDelete('Categories', null, {});
+     
+  }
+};
