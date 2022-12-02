9c9846b69f5c1c476a2bb4a4c0fa3cd0900415c7
Marcos891 <MarcosMansilla1989@hotmail.com>
2022-12-02 16:54:48 -0300

    bajando cambios

--- public/Js/usersValidator.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('load', (e) => {
-    const qs = (element) => document.querySelector(element);
-    const qsa = (element) => document.querySelectorAll(element);
-    const $ = (element) => document.getElementById(element);
-
-    let name = $('name');  
-
-})
