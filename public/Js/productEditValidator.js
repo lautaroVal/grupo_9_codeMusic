26b60efc6f8a0047d1f6d7b623a1cae0b7fccf98
lautaroVal <valenzuelalautaro@yahoo.com>
2022-12-02 17:41:43 -0300

    validacion completa profile y productAdd

--- public/Js/productEditValidator.js
+++ public/Js/productEditValidator.js
@@ -110,7 +110,7 @@ window.addEventListener('load', (event) => {
         msgError("msgCategory", "Debe seleccionar una categoría", e)
         break;
       default:
-        $("msgCategory").innerHTML = '';
+        $("msgCategory").innerHTML = null;
         e.target.classList.remove('is-invalid')
         e.target.classList.add('is-valid');
         break;
