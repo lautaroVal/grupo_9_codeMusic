ec5ddc107f011ccbe8d60c2a59c442d1dfeb9012
lautaroVal <valenzuelalautaro@yahoo.com>
2022-12-03 12:57:12 -0300

    login validado, y acomodado stylos generales

--- src/validations/profileValidator.js
+++ src/validations/profileValidator.js
@@ -1,17 +1,15 @@
 const { body } = require('express-validator');
-const bcryptjs = require('bcryptjs');
 
 module.exports = [
     body('firstName')
-    .notEmpty().withMessage("Debe ingresar un nombre.").bail()
-    .isAlpha('en-US').withMessage('Debe ingresar solo letras.').bail()
-    .isLength({ min: 3 }).withMessage('Debe contener 3 caracteres como mínimo.').bail()
-    .isLength({ max: 60 }).withMessage('Debe contener 60 caracteres como máximo.'),
+    .notEmpty().withMessage("El nombre es requerido.").bail()
+    .isAlpha('en-US').withMessage('Solo se permiten caracteres alfabéticos.').bail()
+    .isLength({ min: 3 }).withMessage('El nombre debe tener como mínimo 3 caracteres.').bail()
+    .isLength({ max: 60 }).withMessage('El nombre no puede superar los 60 caracteres.'),
 
     body('lastName')
-    .notEmpty().withMessage("Debe ingresar un apellido.").bail()
-    .isAlpha('en-US').withMessage('Debe ingresar solo letras.').bail()
-    .isLength({ min: 3 }).withMessage('Debe contener 3 caracteres como mínimo.').bail()
-    .isLength({ max: 60 }).withMessage('Debe contener 60 caracteres como máximo.'),
-
+    .notEmpty().withMessage("El apellido es requerido.").bail()
+    .isAlpha('en-US').withMessage('Solo se permiten caracteres alfabéticos.').bail()
+    .isLength({ min: 3 }).withMessage('El apellido debe tener como mínimo 3 caracteres.').bail()
+    .isLength({ max: 60 }).withMessage('El apellido no puede superar los 60 caracteres.'),
 ]
