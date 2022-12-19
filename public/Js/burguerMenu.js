1e5c3ea953a939e901667321f3526eb9b1bfa1c3
Marcos891 <MarcosMansilla1989@hotmail.com>
2022-12-19 16:17:56 -0300

    acomodando css

---
+++ public/Js/burguerMenu.js
@@ -0,0 +1,6 @@
+const navBurguer = document.getElementById("navBurguer")
+
+burguer.addEventListener('click', () =>{
+    navBurguer.classList.toggle('active');
+    navBurguer.classList.toggle('opacity')
+})
